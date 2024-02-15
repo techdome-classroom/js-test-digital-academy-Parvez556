c7e6e2ab46f2e0579606581a092ab975a44193b7
Parvez556 <husainparvez769@gmail.com>
2024-02-15 15:38:47 +0530

    done

--- program1.js
+++ program1.js
@@ -1,11 +1,134 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isValid = function(s) {
     
-};
+// };
+
+// module.exports = { isValid };
+
+// const prompt=require("prompt-sync")();
+// //my code
+// // function isValidInput(input) {
+// //     let stack = [];
+// //     const openingBraces = ['[', '{', '('];
+// //     const closingBraces = [']', '}', ')'];
+
+// //     for (let char of input) {
+// //         if (openingBraces.includes(char)) {
+// //             stack.push(char);
+// //         } else if (closingBraces.includes(char)) {
+// //             let lastOpening = stack.pop();
+// //             if (!lastOpening || openingBraces.indexOf(lastOpening) !== closingBraces.indexOf(char)) {
+// //                 return false;
+// //             }
+// //         }
+// //     }
+
+// //     return stack.length === 0;
+// // }
+
+// // function getUserInput() {
+// //     let userInput = prompt("Enter input using only (), [], {}:");
+
+// //     if (isValidInput(userInput)) {
+// //         console.log("true");
+// //     } else {
+// // //         console.log("false");
+// // //     }
+// // // }
+
+// // // // Call the function to get user input
+// // // getUserInput();
+
+// // //second 
+// // function isValidInput(input) {
+// //     let stack = [];
+// //     const openingBraces = ['{', '[', '('];
+// //     const closingBraces = ['}', ']', ')'];
+
+// //     for (let char of input) {
+// //         if (!['{', '}', '[', ']', '(', ')'].includes(char)) {
+// //             // If any character other than {}, [], () is encountered, return false
+// //             return false;
+// //         }
+
+// //         if (openingBraces.includes(char)) {
+// //             stack.push(char);
+// //         } else if (closingBraces.includes(char)) {
+// //             let lastOpening = stack.pop();
+// //             if (!lastOpening || openingBraces.indexOf(lastOpening) !== closingBraces.indexOf(char)) {
+// //                 return false;
+// //             }
+// //         }
+// //     }
+
+// //     return stack.length === 0;
+// // }
+
+// // function getUserInput() {
+// //     let userInput = prompt("Enter input using only (), [], {}:");
+
+// //     if (isValidInput(userInput)) {
+// //         console.log("True");
+// //     } else {
+// //         console.log("False");
+// //     }
+// // }
+
+// // // Call the function to get user input
+// // getUserInput();
+// //third
+// function isValidInput(input) {
+//     let stack = [];
+//     const openingBraces = ['(', '[', '{'];
+//     const closingBraces = [')', ']', '}'];
+
+//     for (let char of input) {
+//         if (!['(', ')', '[', ']', '{', '}'].includes(char)) {
+//             // If any character other than '()', '[]', '{}' is encountered, return false
+//             return false;
+//         }
+
+//         if (openingBraces.includes(char)) {
+//             stack.push(char);
+//         } else if (closingBraces.includes(char)) {
+//             let lastOpening = stack.pop();
+//             if (!lastOpening || openingBraces.indexOf(lastOpening) !== closingBraces.indexOf(char)) {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     return stack.length === 0;
+// }
+
+// // Examples         // Output: false
+// program1.js
+
+function isValid(input) {
+    const stack = [];
+    const openingBraces = ['(', '[', '{'];
+    const closingBraces = [')', ']', '}'];
+
+    for (let char of input) {
+        if (!['(', ')', '[', ']', '{', '}'].includes(char)) {
+            // If any character other than '()', '[]', '{}' is encountered, return false
+            return false;
+        }
+
+        if (openingBraces.includes(char)) {
+            stack.push(char);
+        } else if (closingBraces.includes(char)) {
+            let lastOpening = stack.pop();
+            if (!lastOpening || openingBraces.indexOf(lastOpening) !== closingBraces.indexOf(char)) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -8,3 +8,43 @@ var romanToInt = function(s) {
 
 
 module.exports={romanToInt}
+// program2.js
+
+// program2.js
+
+// program2.js
+
+// program2.js
+
+// program2.js
+
+function romanToInt(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentSymbolValue = romanValues[s[i]];
+        const nextSymbolValue = romanValues[s[i + 1]];
+
+        if (nextSymbolValue > currentSymbolValue) {
+            // Subtraction case (e.g., IV, IX, XL, XC, CD, CM)
+            result += nextSymbolValue - currentSymbolValue;
+            i++; // Skip the next symbol, as it has already been considered
+        } else {
+            result += currentSymbolValue;
+        }
+    }
+
+    return result;
+}
+
+module.exports = { romanToInt };
